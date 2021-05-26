d1b1dae34ac0e196f223bd0104c42e49fd04450f
jadamo <joedadamo@gmail.com>
2021-05-26 17:10:58 -0500

    Setup initial mandelbrot set viewing

--- Draw-Fractals.js
+++ Draw-Fractals.js
@@ -7,160 +7,39 @@
  * @author Joe Adamo <joedadamo@gmail.com>
  */
 
-/** @global The WebGL context */
-var gl;
 
 /** @global The HTML5 canvas we draw on */
 var canvas;
 
-/** @global A simple GLSL shader program */
-var shaderProgram;
+/** @global ctx object */
+var ctx;
 
 /**@global mandelbrot fractal object */
 var MBrot;
 
-/** @global the scale matrix**/
-var scaleMatrix = glMatrix.mat4.create();
-
-/** @global The factor to scale our image by */
-var scaleFactor = 0.5;
-
- /**
- * Creates a context for WebGL
- * @param {element} canvas WebGL canvas
- * @return {Object} WebGL context
- */
-function createGLContext(canvas) {
-    var names = ["webgl", "experimental-webgl"];
-    var context = null;
-    for (var i=0; i < names.length; i++) {
-      try { context = canvas.getContext(names[i]); } 
-      catch(e) {}
-      if (context) { break; }
-    }
-    if (context) {
-      context.viewportWidth = canvas.width;
-      context.viewportHeight = canvas.height;
-    } else {
-      alert("Failed to create WebGL context!");
-    }
-    return context;
-  }
-
-// check out https://github.com/emoller/WebGL101 for a good reference
-
-/**
- * Loads Shaders
- * @param {string} id ID string for shader to load. Either vertex shader/fragment shader
- */
-function loadShaderFromDOM(id) {
-    var shaderScript = document.getElementById(id);
-    
-    // If we don't find an element with the specified id
-    // we do an early exit 
-    if (!shaderScript) { return null; }
-    
-    // Loop through the children for the found DOM element and
-    // build up the shader source code as a string
-    var shaderSource = "";
-    var currentChild = shaderScript.firstChild;
-    while (currentChild) {
-      if (currentChild.nodeType == 3) { // 3 corresponds to TEXT_NODE
-        shaderSource += currentChild.textContent;
-      }
-      currentChild = currentChild.nextSibling;
-    }
-   
-    var shader;
-    if (shaderScript.type == "x-shader/x-fragment") {
-      shader = gl.createShader(gl.FRAGMENT_SHADER);
-    } else if (shaderScript.type == "x-shader/x-vertex") {
-      shader = gl.createShader(gl.VERTEX_SHADER);
-    } else {return null;}
-   
-    gl.shaderSource(shader, shaderSource);
-    gl.compileShader(shader);
-   
-    if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      alert(gl.getShaderInfoLog(shader));
-      return null;
-    } 
-    return shader;
-  }
-
-/**
-*  Sends matrices to shader
-*/
-function setMatrixUniforms() {
-  gl.uniformMatrix4fv(shaderProgram.sMatrixUniform, false, scaleMatrix);
-}
-
-/**
-* Setup the fragment and vertex shaders
-*/
-function setupShaders() {
-  vertexShader = loadShaderFromDOM("mandelbrot-vs");
-  fragmentShader = loadShaderFromDOM("mandelbrot-fs");
-  
-  shaderProgram = gl.createProgram();
-  gl.attachShader(shaderProgram, vertexShader);
-  gl.attachShader(shaderProgram, fragmentShader);
-  gl.linkProgram(shaderProgram);
-
-  if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)) {
-    alert("Failed to setup shaders");
-  }
-
-  gl.useProgram(shaderProgram);
-  shaderProgram.vertexPositionAttribute = gl.getAttribLocation(shaderProgram, "aVertexPosition");
-  gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-
-  shaderProgram.vertexColorAttribute = gl.getAttribLocation(shaderProgram, "aVertexColor");
-  gl.enableVertexAttribArray(shaderProgram.vertexColorAttribute);
-
-  //matrices
-  shaderProgram.sMatrixUniform = gl.getUniformLocation(shaderProgram, "scaleMatrix");
-}
-
-//-------------------------------------------------------------------------
-/**
- * Populates buffers with data for spheres
- */
-function setupBuffers(){
-  Mbrot = new Mandelbrot();
-}
+var ImageData;
 
 //----------------------------------------------------------------------------------
 /**
- * Draw call that applies matrix transformations and draws to frame
+ * Draw call that draws fractal to the canvas
  */
 function draw() { 
-  gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
-  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
-
-  glMatrix.mat4.identity(scaleMatrix);
-
-  setMatrixUniforms();
-  Mbrot.drawMandelbrot();
+  ImageData = ctx.createImageData(canvas.width, canvas.height);
+  ImageData = Mbrot.drawMandelbrot(ImageData);
+  ctx.putImageData(ImageData, 0, 0);
 }
 
-/**
- * Update simulation parameters and positions every tick
- */
-function animate(){
-}
 
 /**
  * Startup function called from html code to start program.
  */
 function startup() {
-    canvas = document.getElementById("myGLCanvas");
-  gl = createGLContext(canvas);
-  setupShaders("mandelbrot-vs","mandelbrot-fs");
-  setupBuffers();
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-  gl.enable(gl.DEPTH_TEST);
-  tick();
+  canvas = document.getElementById("FractalCanvas");
+  ctx = canvas.getContext('2d');
+
+  Mbrot = new Mandelbrot();
+  ImageData = ctx.createImageData(canvas.width, canvas.height);
+  draw();
 }
 
 //----------------------------------------------------------------------------------
--- Mandelbrot.js
+++ Mandelbrot.js
@@ -18,64 +18,26 @@ class Mandelbrot{
 
     // vertex buffer
     // idx = x*width + y
-    this.points = [];
+    this.A = [];
+    this.B = [];
+    this.Iterations = [];
 
-    this.faces = [];
-    // buffer to hold the colors (based on iterations) 
-    this.colors = [];
-
-    // initialize vertex buffer
+    // initialize points
     this.fillBuffers();
-
-    this.loadBuffers();
   }
 
   fillBuffers(){
-
-    for(var x = 0; x < this.width; x++){
-        for(var y = 0; y < this.height; y++){
+    for(var y = 0; y < this.height; y++){
+        for(var x = 0; x < this.width; x++){
             var a = this.minA + (x / this.width)*(this.maxA - this.minA)
             var b = this.minB + (y / this.height)*(this.maxB - this.minB)
             var iter = this.isBounded(a, b);
 
-            this.points.push(a);
-            this.points.push(b);
-            this.points.push(0);
-
-            //this assignment of colors is temprary - change this later!
-            var r = (iter % 50) / 50.;
-            var g = (iter % 50) / 50.;
-            var b = (iter % 50) / 50.;
-
-            this.colors.push(r); this.colors.push(g); this.colors.push(b);
-            this.colors.push(0); //alpha = 0 always
+            this.A.push(a);
+            this.B.push(b);
+            this.Iterations.push(iter);
         }
     }
-    //load triangle faces themselves (needs to be done after the above)
-    for (var i = 0; i < this.width; i++){
-        for (var j = 0; j < this.height; j++){
-
-            var idx = i*(this.height+1)+j;
-
-            //2 triangles per square
-            this.faces.push(idx);
-            this.faces.push(idx+1);
-            this.faces.push(idx+this.height+1);
-
-            this.faces.push(idx+1);
-            this.faces.push(idx+1+this.height+1);
-            this.faces.push(idx+this.height+1);
-        }
-    }
-  }
-
-  /**
-  * Finds the absolute distance from the origin for a given complex number
-  * @param {number} real component (a) of a + bi
-  * @param {number} imaginary component (b) of a + bi
-  */
-  abs(a, b){
-      return Math.pow(a, 2) + Math.pow(b, 2);
   }
 
   /**
@@ -94,7 +56,7 @@ class Mandelbrot{
           // f(z+1) = z^2 + c, where c is the original point and z is iterated and z(0) = 0
           var a1 = ((a * a) - (b * b)) + cA;
           var b1 = (2 * a * b) + cB;
-          if(this.abs(a1, b1) >= 4.0){
+          if(Math.pow(a1, 2) + Math.pow(b1, 2) >= 4.0){
               return i;
           }
           i++; a = a1; b = b1;
@@ -103,43 +65,27 @@ class Mandelbrot{
     }
 
     /**
-    * Send the buffer objects to WebGL for rendering 
+    * loads in color data to a canvas ImageData object for drawing
+    * @param {ImageData object} ImageData 
+    * @return {ImageData object} ImageData based on Mandelbrot values
     */
-    loadBuffers(){
-        // Specify the vertex coordinates
-        this.VertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexBuffer);      
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.points), gl.STATIC_DRAW);
-        this.VertexBuffer.itemSize = 3;
-        this.VertexBuffer.numItems = this.width * this.height;
-        console.log("Loaded ", this.VertexBuffer.numItems, " vertices");
+    drawMandelbrot(ImageData){
+        for(var x = 0; x < this.width; x++){
+            for(var y = 0; y < this.height; y++){
+                var idx1 = y * (this.height) + x
+                var idx2 = y * (this.width * 4) + x * 4;
 
-         // Specify faces of the terrain 
-         this.IndexTriBuffer = gl.createBuffer();
-         gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
-         gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint32Array(this.faces),
-                   gl.STATIC_DRAW);
-         this.IndexTriBuffer.itemSize = 1;
-         this.IndexTriBuffer.numItems = this.faces.length;
-         console.log("Loaded ", this.IndexTriBuffer.numItems, " triangles");
+                //this assignment of colors is temprary - change this later!
+                var r = ((this.Iterations[idx1]*3) % 255);
+                var g = ((this.Iterations[idx1]*3) % 255);
+                var b = ((this.Iterations[idx1]*3) % 255);
 
-        this.ColorBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.ColorBuffer);
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(this.colors), gl.DYNAMIC_DRAW);
-        this.ColorBuffer.itemSize = 4;
-        this.ColorBuffer.numItems = this.VertexBuffer.numItems;
-    }
-
-    drawMandelbrot(){
-        //console.log(this.points[0], " ", this.points[1], " ", this.points[2]);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.VertexBuffer);
-		gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, this.VertexBuffer.itemSize, 
-                             gl.FLOAT, false, 0, 0);
-        gl.bindBuffer(gl.ARRAY_BUFFER, this.ColorBuffer);
-        gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
-                                this.ColorBuffer.itemSize, gl.FLOAT, false, 0, 0);
-        
-        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.IndexTriBuffer);
-        gl.drawArrays(gl.TRIANGLE, 0, this.IndexTriBuffer.numItems); 
+                ImageData.data[idx2]   = r;
+                ImageData.data[idx2+1] = g;
+                ImageData.data[idx2+2] = b;
+                ImageData.data[idx2+3] = 255;
+            }
+        }
+    return ImageData;
     }
 }
